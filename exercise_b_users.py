833da208d8b73ebe01afdd5dd068a81fa5debab7
Nadia Al-Murshedy <nadia@Nadias-MacBook-Air.local>
2022-05-03 23:08:07 +0100

    homework

---
+++ exercise_b_users.py
@@ -0,0 +1,105 @@
+from re import U
+
+
+users = {
+  "Jonathan": {
+    "twitter": "jonnyt",
+    "lottery_numbers": [6, 12, 49, 33, 45, 20],
+    "home_town": "Stirling",
+    "pets": [
+    {
+      "name": "fluffy",
+      "species": "cat"
+    },
+    {
+      "name": "fido",
+      "species": "dog"
+    },
+    {
+      "name": "spike",
+      "species": "dog"
+    }
+  ]
+  },
+  "Erik": {
+    "twitter": "eriksf",
+    "lottery_numbers": [18, 34, 8, 11, 24],
+    "home_town": "Linlithgow",
+    "pets": [
+    {
+      "name": "nemo",
+      "species": "fish"
+    },
+    {
+      "name": "kevin",
+      "species": "fish"
+    },
+    {
+      "name": "spike",
+      "species": "dog"
+    },
+    {
+      "name": "rupert",
+      "species": "parrot"
+    }
+  ]
+  },
+  "Avril": {
+    "twitter": "bridgpally",
+    "lottery_numbers": [12, 14, 33, 38, 9, 25],
+    "home_town": "Dunbar",
+    "pets": [
+      {
+        "name": "monty",
+        "species": "snake"
+      }
+    ]
+  }
+}
+
+# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+
+print(users["Jonathan"]["twitter"])
+
+# 2. Get Erik's hometown
+
+print(users["Erik"]["home_town"])
+
+# 3. Get the list of Erik's lottery numbers
+
+print(users["Erik"]["lottery_numbers"])
+
+# 4. Get the species of Avril's pet Monty
+
+print(users["Avril"]["pets"][0]["species"])
+
+# 5. Get the smallest of Erik's lottery numbers
+
+print(min(users["Erik"]["lottery_numbers"]))
+
+# 6. Return an list of Avril's lottery numbers that are even
+
+num = 0
+
+while(num < len(users["Avril"]["lottery_numbers"])):
+  if (users["Avril"]["lottery_numbers"])[num] % 2 == 0:
+    print((users["Avril"]["lottery_numbers"])[num], end=" ")
+
+  num += 1
+
+# 7. Erik is one lottery number short! Add tshe number `7` to be included in his lottery numbers
+
+users["Erik"]["lottery_numbers"].append("7")
+
+# 8. Change Erik's hometown to Edinburgh
+
+users["Erik"]["home_town"] = ["Edinburgh"]
+
+# 9. Add a pet dog to Erik called "fluffy"
+
+erik_new_pet = {"name":"fluffy", "species":"dog"}
+users["Erik"]["pets"].append(erik_new_pet)
+
+# 10. Add another person to the users dictionary
+
+users.update({"Temperance": {"twitter": "tbrennan", "lottery_numbers": "[5, 14, 41, 23]", "pets": {"name": "david", "species": "spider"}}})
